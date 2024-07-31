bf302ad6ad17082419f19b4baa4181cf0572e83c
Hiroaki Yutani <yutani.ini@gmail.com>
2024-07-31 13:59:57 +0900

    Parsing unary operators

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,40 @@ fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
         .map(|s: String| Expr::Num(s.parse().unwrap()))
         .padded();
 
-    int.then_ignore(end())
+    let atom = int;
+
+    let op = |c| just(c).padded();
+
+    let unary = op('-')
+        .repeated()
+        .then(atom)
+        .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
+
+    unary.then_ignore(end())
+}
+
+fn eval(expr: &Expr) -> Result<f64, String> {
+    match expr {
+        Expr::Num(x) => Ok(*x),
+        Expr::Neg(a) => Ok(-eval(a)?),
+        Expr::Add(a, b) => Ok(eval(a)? + eval(b)?),
+        Expr::Sub(a, b) => Ok(eval(a)? - eval(b)?),
+        Expr::Mul(a, b) => Ok(eval(a)? * eval(b)?),
+        Expr::Div(a, b) => Ok(eval(a)? / eval(b)?),
+        _ => todo!(), // We'll handle other cases later
+    }
 }
 
 fn main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
 
-    println!("{:?}", parser().parse(src));
+    match parser().parse(src) {
+        Ok(ast) => match eval(&ast) {
+            Ok(output) => println!("ast:  {ast:?}\neval: {output}"),
+            Err(eval_err) => println!("Evaluation error: {}", eval_err),
+        },
+        Err(parse_errs) => parse_errs
+            .into_iter()
+            .for_each(|e| println!("Parse error: {e}")),
+    }
 }
