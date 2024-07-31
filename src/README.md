1e4645d7ed0927eab0ee4169a121381ce2211640
Hiroaki Yutani <yutani.ini@gmail.com>
2024-07-31 12:16:28 +0900

    Initial commit

---
+++ src/README.md
@@ -0,0 +1,4 @@
+This is just my personal practice of [Chumsky](https://github.com/zesterer/chumsky)
+========
+
+虚無が好き。
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use chumsky::prelude::*;
+
+fn main() {
+    let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
+
+    println!("{}", src);
+}
