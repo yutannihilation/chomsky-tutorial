4c485b68f0b747f7457427982082f527d8212836
Hiroaki Yutani <yutani.ini@gmail.com>
2024-07-31 13:31:00 +0900

    Parsing digits

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 use chumsky::prelude::*;
 
+#[derive(Debug)]
+enum Expr {
+    Num(f64),
+    Var(String),
+
+    Neg(Box<Expr>),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+
+    Call(String, Vec<Expr>),
+    Let {
+        name: String,
+        rhs: Box<Expr>,
+        then: Box<Expr>,
+    },
+    Fn {
+        name: String,
+        args: Vec<String>,
+        body: Box<Expr>,
+        then: Box<Expr>,
+    },
+}
+
+fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
+    filter(char::is_ascii_digit).map(|c| Expr::Num(c.to_digit(10).unwrap() as f64))
+}
+
 fn main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
 
-    println!("{}", src);
+    println!("{:?}", parser().parse(src));
 }
