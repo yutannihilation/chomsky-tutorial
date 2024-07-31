0d72f91cb74e217b7ebf1f09de756edb8a96e536
Hiroaki Yutani <yutani.ini@gmail.com>
2024-07-31 15:14:48 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -26,33 +26,59 @@ enum Expr {
 }
 
 fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
-    let int = text::int(10)
-        .map(|s: String| Expr::Num(s.parse().unwrap()))
-        .padded();
+    let ident = text::ident().padded();
 
-    let atom = int;
+    let expr = recursive(|expr| {
+        let int = text::int(10)
+            .map(|s: String| Expr::Num(s.parse().unwrap()))
+            .padded();
 
-    let op = |c| just(c).padded();
+        let atom = int
+            .or(expr.delimited_by(just('('), just(')')))
+            .or(ident.map(Expr::Var));
 
-    let unary = op('-')
-        .repeated()
-        .then(atom)
-        .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
+        let op = |c| just(c).padded();
 
-    let mul = op('*').to(Expr::Mul as fn(_, _) -> _);
-    let div = op('/').to(Expr::Div as fn(_, _) -> _);
-    let add = op('+').to(Expr::Add as fn(_, _) -> _);
-    let sub = op('-').to(Expr::Sub as fn(_, _) -> _);
+        let unary = op('-')
+            .repeated()
+            .then(atom)
+            .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
 
-    let product = unary
-        .then(mul.or(div).then(unary).repeated())
-        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+        let mul = op('*').to(Expr::Mul as fn(_, _) -> _);
+        let div = op('/').to(Expr::Div as fn(_, _) -> _);
+        let add = op('+').to(Expr::Add as fn(_, _) -> _);
+        let sub = op('-').to(Expr::Sub as fn(_, _) -> _);
 
-    let sum = product
-        .then(add.or(sub).then(product).repeated())
-        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+        let product = unary
+            .clone()
+            .then(mul.or(div).then(unary).repeated())
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
 
-    sum.then_ignore(end())
+        let sum = product
+            .clone()
+            .then(add.or(sub).then(product).repeated())
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+        sum
+    })
+    .then_ignore(end());
+
+    let decl = recursive(|decl| {
+        let r#let = text::keyword("let")
+            .ignore_then(ident)
+            .then_ignore(just('='))
+            .then(expr.clone())
+            .then_ignore(just(';'))
+            .then(decl)
+            .map(|((name, rhs), then)| Expr::Let {
+                name,
+                rhs: Box::new(rhs),
+                then: Box::new(then),
+            });
+        r#let.or(expr).padded()
+    });
+
+    decl.then_ignore(end())
 }
 
 fn eval(expr: &Expr) -> Result<f64, String> {
